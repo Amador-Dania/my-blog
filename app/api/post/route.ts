e284b485b63c849795ca3c2534fd5f22461fd79a
Dani <amadordaniela@proton.me>
2024-01-28 20:47:15 -0800

    backend set

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,58 +1,27 @@
-import getDb from "../../utils.ts/db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import db from "@/app/utils.ts/getDb";
 
-import Post from "../../types/post";
-
-const db = getDb();
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("hello");
-
-  switch (req.method) {
-    case "GET":
-      await GET(res);
-      break;
-    case "POST":
-      await POST(req, res);
-      break;
-    default:
-      res.status(405).end(); //Method Not Allowed
-      break;
-  }
+export async function GET() {
+  const res = await db.many("SELECT * FROM posts");
+  return Response.json({ res });
 }
 
-export async function GET(res: NextApiResponse) {
-  //   const posts: Post[] = db.one("SELECT * FROM posts WHERE id = 1");
-  //   res.json(posts);
+export async function POST(req: Request) {
+  const data = await req.json();
+  const result = await db.one(
+    "INSERT INTO posts (title, author, publication_date, content) VALUES(${title}, ${author}, ${publication_date}, ${content}) RETURNING *",
+    {
+      title: data.title,
+      author: data.author,
+      publication_date: data.publicationDate,
+      content: data.content,
+    }
+  );
 
-  try {
-    const entries = await db.any("SELECT * FROM posts");
-    res.status(200).json(entries);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error retrieving entries " });
-  }
+  console.log(result);
+
+  // We can use every supported variable syntax at the same time, if needed:
+
+  return Response.json({
+    data,
+  });
 }
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  //   const posts = db.one("SELECT * FROM posts WHERE id = 1");
-  //   res.json(posts);
-
-  const { title, author, publication_date, content } = req.body;
-  try {
-    await db.none(
-      "INSERT INTO posts (title, author, publication_date, content) VALUES ($1, $2, $3, $4)",
-      [title, author, publication_date, content]
-    );
-    res.status(201).json({ message: "Entry created" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error creating entry" });
-  }
-}
-
-// localhost:3000/api/post
-// POST api/posts
--- app/utils.ts/getDb.ts
+++ app/utils.ts/getDb.ts
@@ -1,26 +1,15 @@
 import pgPromise from "pg-promise";
 
-export default async function getDb() {
-  // in production, we would use environment variables instead of hardcoding the values
-  const dbConfig = {
-    host: "localhost",
-    port: 5432,
-    database: "myblog",
-    user: "postgres",
-    password: "postgres",
-  };
+const dbConfig = {
+  host: "localhost",
+  port: 5432,
+  database: "myblog",
+  user: "postgres",
+  password: "postgres",
+};
 
-  const pgPromiseInitialized = pgPromise();
+const pgPromiseInitialized = pgPromise();
 
-  const db = pgPromiseInitialized(dbConfig);
+const db = pgPromiseInitialized(dbConfig);
 
-  try {
-    const connection = await db.connect();
-    console.log("Connected to the database");
-    connection.done(); // release connection
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-  }
-
-  return db;
-}
+export default db;
